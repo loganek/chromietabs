d1d1820cc8d8fdae8c980a6cd82bea7dc1211c30
Marcin Kolny <marcin.kolny@gmail.com>
2017-01-07 14:45:47 +0100

    add documentation generator

---
+++ doc-generator.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Expected build directory"
+    exit 1
+fi
+
+tag=$(git describe --exact-match --tags HEAD)
+
+if [ $? -ne 0 ]; then
+    tag=master
+fi
+
+build_path=`cd "$1"; pwd`
+
+prev_dir=`pwd`
+cd $build_path
+make documentation
+
+git clone https://github.com/loganek/chromietabs chromietabs-tmp
+cd chromietabs-tmp
+git checkout gh-pages
+rm * -rf $tag
+mkdir $tag
+cp ../docs/references/html/* $tag/ -r
+
+echo '<html><head><title>chromietabs documentation</title></head><body><ul>' > index.html
+
+for d in */ ; do
+    echo "<li><a href='$d/index.html'>$d</a>" >> index.html
+done
+echo '</ul></body></html>' >> index.html
+
+git add .
+git commit -m"update documentation for $tag"
+git push origin gh-pages
+cd ..
+rm -rf chromietabs-tmp
+cd $prev_dir
